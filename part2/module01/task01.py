1bb340a8658792d44156d92b0d37ecac0ad3f3b7
Defalt1878 <ilyaudalov1@mail.ru>
2022-10-31 14:53:09 +0500

    Part 2 subfolders renamed

--- part2/module01/task01.py
+++ part2/module01/task01.py
@@ -10,5 +10,5 @@ info = 'OS info is \n{}\n\nPython version is {} {}'.format(
 )
 print(info)
 
-with open('module1/os_info.txt', 'w', encoding='utf8') as file:
+with open('module01/os_info.txt', 'w', encoding='utf8') as file:
     file.write(info)
